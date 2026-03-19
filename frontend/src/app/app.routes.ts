25c228a321b0d4d1ebdef56c5635a47fc714bf77
Valerii Navalnyi <valeranavalni@gmail.com>
2026-03-19 22:06:44 +0000

    link routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './routes/home/home';
+import { About } from './routes/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: Home },
+    { path: "about", component: About }
+];
