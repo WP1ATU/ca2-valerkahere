bc579af7add7f6be0f916d4988c70234ccf4a1a0
Valerii Navalnyi <valeranavalni@gmail.com>
2026-03-19 22:08:14 +0000

    link home to additem, listitems

--- frontend/src/app/routes/home/home.ts
+++ frontend/src/app/routes/home/home.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { Additem } from '../../components/additem/additem';
+import { Listitems } from '../../components/listitems/listitems';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    Additem, Listitems
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
